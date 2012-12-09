582524425c928b4af93ec09358270c81a36ad355
Jason Webb <bigjasonwebb@gmail.com>
2012-12-08 23:28:45 -0700

    Refactor the config

--- lib/midge/config.rb
+++ lib/midge/config.rb
@@ -6,17 +6,16 @@ module Midge
     attr_reader :js_null_processor_enabled
 
     def initialize
-      @jst_processors = []
-      @js_processors = []
+      @processors = []
       set_js_null_processor_enabled(true)
     end
 
     def jst_processor(extension, namespace=Rails.application.class.parent_name)
-      @jst_processors << [extension, namespace]
+      @processors << [::Midge::JstProcessor, extension, namespace]
     end
 
     def js_processor(extension, namespace=Rails.application.class.parent_name)
-      @js_processors << [extension, namespace]
+      @processors << [::Midge::JavascriptProcessor, extension, namespace]
     end
 
     def set_js_null_processor_enabled(value)
@@ -24,11 +23,8 @@ module Midge
     end
 
     def configure!(sprockets=Sprockets)
-      @js_processors.each do |extension, namespace|
-        sprockets.register_engine extension, create_processer(::Midge::JavascriptProcessor, namespace)
-      end
-      @jst_processors.each do |extension, namespace|
-        sprockets.register_engine extension, create_processer(::Midge::JstProcessor, namespace)
+      @processors.each do |base_klass, extension, namespace|
+        sprockets.register_engine extension, create_processer(base_klass, namespace)
       end
     end
 
